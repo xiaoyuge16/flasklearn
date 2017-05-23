61f377ef2b93505155350da2ad3d33b086c220a3
xiaoyuge161108 <862558909@qq.com>
2017-05-23 21:22:37 +0800

    render

--- app/hello.py
+++ app/hello.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
+from flask_bootstrap import Bootstrap
+bootstrap=Bootstrap(app)
 
-@app.route('/')
-def index():
-    return '<h1>hello flask</h1>'
+#@app.route('/')
+#def index():
+#    return '<h1>hello flask</h1>'
 @app.route('/user/<name>')
 def user(name):
     return '<h1>hello,%s</h1>'%name
+@app.route('/')
+def indexr():
+    return render_template('base.html')
 
 
 if __name__=='__main__':
