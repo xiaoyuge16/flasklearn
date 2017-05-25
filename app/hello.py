97c64d0b121c3e9c80bdb09a4ed9b2a217cbfdab
xiaoyuge16 <862558909@qq.com>
2017-05-25 22:14:52 +0800

    post

--- app/hello.py
+++ app/hello.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 #from flask_bootstrap import Bootstrap
 #bootstrap=Bootstrap(app)
 from flask import request
-
+from werkzeug import secure_filename
 
 #def index():
 #    return '<h1>hello flask</h1>'
@@ -19,7 +19,8 @@ def index():
 def upload():
     if request.method == 'POST':
         f = request.files['the_file']
-        f.save('/var/www/uploads/uploaded_file.txt')
+        return '<h1>%s</h1>'%f.filename
+        f.save(secure_filename(f.filename))
     return render_template('upload.html')
 if __name__=='__main__':
     app.run(host='0.0.0.0')
