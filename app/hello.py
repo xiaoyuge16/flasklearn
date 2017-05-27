d2051d88756b0517b9f036cfcff71ca87cd275f4
xiaoyuge161108 <862558909@qq.com>
2017-05-27 10:21:38 +0800

    post

--- app/hello.py
+++ app/hello.py
@@ -18,9 +18,9 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        f = request.files['the_file']
+        f = request.files['file']
         return '<h1>%s</h1>'%f.filename
         f.save(secure_filename(f.filename))
     return render_template('upload.html')
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port='5000')
