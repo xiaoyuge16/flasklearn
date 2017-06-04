7142a89bdcbdfc8f059dfba5a9bf82d14c59f7b3
xiaoyuge16 <862558909@qq.com>
2017-06-04 22:42:07 +0800

    完成了导航栏的功能（flask-nav），并实现了form页面的flash消息闪现功能，但是不知flash消息为何显示了两次

--- app/first.py
+++ app/first.py
@@ -5,7 +5,7 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+app.config['SECRET_KEY']='haosy323'
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!,Welcome!</h1>'
@@ -14,10 +14,11 @@ def hello_world():
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    # if request.method == 'POST':
-        # flash(u'你的提交方法是POST')
+    if request.method == 'POST':
+        flash(u'你的提交方法是POST')
+        return render_template('form.html', method=request.method)
     return render_template('form.html', method=request.method)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug='True')
